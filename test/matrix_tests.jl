6007b0c044def83b4ac5a8cc2a91d6a48449fc92
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-10-19 19:57:02 -0400

    Agglomerative tests

--- test/matrix_tests.jl
+++ test/matrix_tests.jl
@@ -7,9 +7,3 @@ function test_initialize_counts(M::Array{Int64, 2}, g::SimpleWeightedDiGraph)
         @test M[dst(edge), src(edge)] == 1
     end
 end
-
-#test_initialize_counts(10)
-
-#partition(Array{Int64, 2}, "emergingEdges", 1000)
-
-#partition(Array{Int64, 2}, 1000)
--- test/partition_tests.jl
+++ test/partition_tests.jl
@@ -1,13 +1,4 @@
-
-function test_initialize_counts(
-        T::Type, g::SimpleWeightedDiGraph, num_nodes::Int64
-    )
-    M = initialize_edge_counts(
-        T, g, num_nodes, collect(1:num_nodes)
-    )
-    test_initialize_counts(M, g)
-    M
-end
+import GraphChallenge: compute_block_neighbors_and_degrees
 
 function test_compute_block_degrees(M, g::SimpleWeightedDiGraph, num_nodes::Int64)
     d_out, d_in, d = compute_block_degrees(M, num_nodes)
@@ -21,9 +12,31 @@ end
 
 @testset "Initialization" begin
     for T in (Array{Int64, 2}, )
-        num_nodes = 10
-        g = SimpleWeightedDiGraph(LightGraphs.CompleteDiGraph(num_nodes))
-        M = test_initialize_counts(T, g, num_nodes)
+        num_nodes = 50
+        g = load_graph(50)
+        M = initialize_edge_counts(
+            T, g, num_nodes, collect(1:num_nodes)
+        )
+        test_initialize_counts(M, g)
         test_compute_block_degrees(M, g, num_nodes)
+    end
+end
+
+@testset "Agglomerative step" begin
+    for T in (Array{Int64, 2}, )
+        num_nodes = 50
+        g = load_graph(50)
+        M = initialize_edge_counts(
+            T, g, num_nodes, collect(1:num_nodes)
+        )
+        d_out, d_in, d = compute_block_degrees(M, num_nodes)
+        current_block = 1
+        block_neighbors, k_out, k_in, k = compute_block_neighbors_and_degrees(
+            M, current_block
+        )
+        @test Set(all_neighbors(g, current_block)) == Set(block_neighbors)
+        @test k_out == outdegree(g, current_block)
+        @test k_in == indegree(g, current_block)
+        @test k == degree(g, current_block)
     end
 end
