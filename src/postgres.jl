f43876d157f99dee7285fa8a3d2ed3e8dbe80711
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2018-01-24 23:13:54 -0500

    Partitioning running till update

--- src/postgres.jl
+++ src/postgres.jl
@@ -1,4 +1,4 @@
-using NamedTuples, DataStreams, LibPQ
+using NamedTuples, DataStreams, LibPQ, Missings
 
 export InterblockEdgeCountPostgres
 
@@ -7,7 +7,7 @@ const edgelist_tuple = @NT(block_num::Array{Int32}, src_block::Array{Int32}, dst
 immutable InterblockEdgeCountPostgres
     conn::Connection
 
-    function InterblockEdgeCountPostgres(;dbname::String="postgres")
+    function InterblockEdgeCountPostgres(;dbname::String="rohitvarkey")
         conn = Connection("dbname=$dbname")
         edgelist_create_stmt = """CREATE TABLE edgelist (
             block_num   integer NOT NULL,
@@ -89,7 +89,9 @@ function initialize_edge_counts(
     M
 end
 
-function compute_block_neighbors_and_degrees(p::Partition{InterblockEdgeCountPostgres}, block::Int64)
+function compute_block_neighbors_and_degrees(
+    p::Partition{InterblockEdgeCountPostgres}, block::Int64, count_log::CountLog
+    )
     out_neighbors = fetch!(
         NamedTuple,
         execute(p.M.conn, "SELECT dst_block, edgecount FROM edgelist WHERE src_block = $block and block_num=$(p.B);")
@@ -97,6 +99,10 @@ function compute_block_neighbors_and_degrees(p::Partition{InterblockEdgeCountPos
     in_neighbors = fetch!(
         NamedTuple,
         execute(p.M.conn, "SELECT src_block, edgecount FROM edgelist WHERE dst_block = $block and block_num=$(p.B);")
+    )
+    neighbors = collect(Int64,
+        Set(skipmissing(out_neighbors[:dst_block])) ∪
+        Set(skipmissing(in_neighbors[:src_block]))
     )
     k_in = sum(out_neighbors[:edgecount])
     k_out = sum(in_neighbors[:edgecount])
@@ -159,11 +165,11 @@ function compute_new_matrix(
             NamedTuple,
             execute(p.M.conn, "SELECT src_block, edgecount FROM edgelist WHERE dst_block = $block and block_num=$(p.B);")
         )
-        for idx=1:len(out_neighbors)
-            col[out_neighbors[:dst_block][idx]] = out_neighbors[:edgecount][idx]
+        for (dst_block, edgecount) in zip(out_neighbors[:dst_block], out_neighbors[:edgecount])
+            col[dst_block] = edgecount
         end
-        for idx=1:len(in_neighbors)
-            row[in_neighbors[:src_block][idx]] = in_neighbors[:edgecount][idx]
+        for (src_block, edgecount) in zip(in_neighbors[:src_block], in_neighbors[:edgecount])
+            row[src_block] = edgecount
         end
     end
 
@@ -202,14 +208,14 @@ end
 """Computes the new rows and cols in `M`, when all nodes from `r` are shifted to
 block `s`."""
 function compute_new_matrix_agglomerative(
-    p::Partition{InterblockEdgeCountPostgres}, r::Int64, s::Int64
+    p::Partition{InterblockEdgeCountPostgres}, r::Int64, s::Int64, count_log::CountLog
     )
 
     M_r_row = zeros(Int64, p.B)
     M_r_col = zeros(Int64, p.B)
 
-    M_s_row = copy(p.M[s, :])
-    M_s_col = copy(p.M[:, s])
+    M_s_row = zeros(Int64, p.B)
+    M_s_col = zeros(Int64, p.B)
 
     # Setup with initial counts
     for block in (r, s)
@@ -225,10 +231,10 @@ function compute_new_matrix_agglomerative(
             if dst_block == r
                 M_s_row[s] += edgecount
             else
-                M_s_col[:dst_block] += edgecount
+                M_s_col[dst_block] += edgecount
             end
         end
-        for (src_block, edgecount) in zip(in_neighbors[:dst_block], in_neighbors[:edgecount])
+        for (src_block, edgecount) in zip(in_neighbors[:src_block], in_neighbors[:edgecount])
             if src_block == r
                 M_s_row[s] += edgecount
             else
@@ -244,7 +250,7 @@ function compute_new_matrix_agglomerative(
 end
 
 function compute_multinomial_probs(
-    p::Partition{InterblockEdgeCountPostgres}, vertex::Int64
+    p::Partition{InterblockEdgeCountPostgres}, vertex::Int64, count_log::CountLog
     )
     out_counts = fetch!(
         NamedTuple,
@@ -269,6 +275,59 @@ function compute_multinomial_probs(
         end
     end
     return probs/p.d[vertex]
+end
+
+function compute_delta_entropy(
+    p::Partition{InterblockEdgeCountPostgres}, r::Int64, s::Int64,
+    M_r_col::Array{Int64, 1}, M_s_col::Array{Int64, 1},
+    M_r_row::Array{Int64, 1}, M_s_row::Array{Int64, 1},
+    d_out_new::Vector{Int64}, d_in_new::Vector{Int64}, count_log::CountLog
+    )
+    delta = 0.0
+    # Sum over col of r in new M
+    for t1 in findn(M_r_col)
+        # Skip if t1 is r or s to prevent double counting
+        if t1 ∈ (r, s)
+            continue
+        end
+        #@show t1, M_r_col[t1], d_in_new[t1], d_out_new[r]
+        delta -= M_r_col[t1] * log(M_r_col[t1] / d_in_new[t1] / d_out_new[r])
+    end
+    for t1 in findn(M_s_col)
+        if t1 ∈ (r, s)
+            continue
+        end
+        #@show t1, M_s_col[t1], d_in_new[t1], d_out_new[s]
+        delta -= M_s_col[t1] * log(M_s_col[t1] / d_in_new[t1] / d_out_new[s])
+    end
+    # Sum over row of r in new M
+    for t2 in findn(M_r_row)
+        #@show t2, M_r_row[t2], d_in_new[r], d_out_new[t2]
+        delta -= M_r_row[t2] * log(M_r_row[t2] / d_in_new[r] / d_out_new[t2])
+    end
+    # Sum over row of s in new M
+    for t2 in findn(M_s_row)
+        #@show t2, M_s_row[t2], d_in_new[s], d_out_new[t2]
+        delta -= M_s_row[t2] * log(M_s_row[t2] / d_in_new[s] / d_out_new[t2])
+    end
+
+    # Sum over edges in old M
+    edges_query = fetch!(
+        NamedTuple,
+        execute(p.M.conn, """
+        SELECT src_block, dst_block, edgecount FROM edgelist WHERE
+        block_num=$(p.B) and src_block IN ($r, $s) or dst_block IN ($r, $s);
+        """
+        )
+    )
+    for (src_block, dst_block, edgecount) in zip(
+        edges_query[:src_block],
+        edges_query[:dst_block],
+        edges_query[:edgecount]
+    )
+        delta += edgecount * log(edgecount / p.d_in[dst_block] / p.d_out[src_block])
+    end
+    delta
 end
 
 function compute_overall_entropy(
@@ -296,4 +355,54 @@ function compute_overall_entropy(
     model_S = E * (1 + model_S_term) * log(1 + model_S_term) -
         model_S_term * log(model_S_term) + N*log(B)
     S = model_S + summation_term
+end
+
+function compute_hastings_correction(
+        s::Int64, p::Partition{InterblockEdgeCountPostgres}, M_r_row::Vector{Int64},
+        M_r_col::Vector{Int64}, d_new::Vector{Int64},
+        blocks_out_count_map, blocks_in_count_map, count_log::CountLog
+    )
+    blocks_out = collect(keys(blocks_out_count_map))
+    blocks_in = collect(keys(blocks_in_count_map))
+    blocks = Set(blocks_out) ∪ Set(blocks_in)
+
+    out_counts_query = fetch!(
+        NamedTuple,
+        execute(p.M.conn, """
+        SELECT src_block as neighbor, edgecount FROM edgelist WHERE
+        src_block in ($(join(blocks_in, ','))) and block_num=$(p.B) and dst_block=$s;
+        """
+        )
+    )
+    in_counts_query = fetch!(
+        NamedTuple,
+        execute(p.M.conn, """
+        SELECT dst_block as neighbor, edgecount FROM edgelist WHERE
+        dst_block in ($(join(blocks_out, ','))) and block_num=$(p.B) and src_block=$s;
+        """
+        )
+    )
+    out_counts = Dict{Int64, Int64}()
+    in_counts = Dict{Int64, Int64}()
+
+    for (neighbor, edgecount) in zip(out_counts_query[:neighbor], out_counts_query[:edgecount])
+        out_counts[neighbor] = edgecount
+    end
+    for (neighbor, edgecount) in zip(in_counts_query[:neighbor], in_counts_query[:edgecount])
+        in_counts[neighbor] = edgecount
+    end
+    p_forward = 0.0
+    p_backward = 0.0
+    for t in blocks
+        degree = get(blocks_out_count_map, t, 0) + get(blocks_in_count_map, t, 0)
+        edgecounts = get(out_counts, t, 0) + get(in_counts, t, 0)
+        p_forward += degree * (edgecounts + 1) / (p.d[t] + p.B)
+        p_backward += degree * (M_r_row[t] + M_r_col[t] + 1) / (d_new[t] + p.B)
+    end
+    return p_backward / p_forward
+end
+
+function zeros_interblock_edge_matrix(::Type{InterblockEdgeCountPostgres},
+    size::Int64)
+    return InterblockEdgeCountPostgres()
 end
