ebdc072ab4dadd23244d4e6acd6bf2e55163822c
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2018-01-30 21:47:39 -0500

    Refactor for new generic partition structure

--- src/interblock_matrix.jl
+++ src/interblock_matrix.jl
@@ -5,25 +5,33 @@ Calculates the new out, in and total degrees for the updated edge count matrix.
 Returns a tuple of M, d_out, d_in, d
 """
 function initialize_edge_counts!(
-    M::Array{Int64, 2}, g::SimpleWeightedDiGraph, B::Int64, b::Vector{Int64}
+    M::Array{Int64, 2}, g::SimpleWeightedDiGraph, B::Int64, b::Vector{Int64},
+    count_log::CountLog
     )
     @assert size(M) == (B, B)
     for edge in edges(g)
         M[b[dst(edge)], b[src(edge)]] += weight(edge)
     end
+    count_log.edges_inserted += ne(g)
 end
 
-function compute_block_neighbors_and_degrees(p::Partition{Array{Int64, 2}}, block::Int64)
+function compute_block_neighbors_and_degrees(
+    p::Partition{Array{Int64, 2}},
+    block::Int64,
+    count_log::CountLog
+    )
     out_neighbors = findn(p.M[:, block])
     in_neighbors = findn(p.M[block, :])
     neighbors = collect(Set(out_neighbors) ∪ Set(in_neighbors))
     k_in = sum(p.M[block, in_neighbors])
     k_out = sum(p.M[out_neighbors, block])
     k = k_out + k_in
+    count_log.edges_traversed += length(out_neighbors) + length(in_neighbors)
     neighbors, k_out, k_in, k
 end
 
-function compute_block_degrees(M::Array{Int64, 2}, B::Int64)
+function compute_block_degrees(M::Array{Int64, 2}, B::Int64, count_log::CountLog)
+    #TODO: Figure out how to count edges traversed here.
     # Sum across rows to get the outdegrees for each block
     d_out = reshape(sum(M, 1), B)
     # Sum across cols to get the indegrees for each block
@@ -34,17 +42,19 @@ end
 
 function initialize_edge_counts(
     _::Type{Array{Int64, 2}}, g::SimpleWeightedDiGraph, B::Int64,
-    b::Vector{Int64}
+    b::Vector{Int64}, count_log::CountLog
     )
     M = zeros(Int64, B, B)
-    initialize_edge_counts!(M, g, B, b)
+    initialize_edge_counts!(M, g, B, b, count_log)
     M
 end
 
 function compute_new_matrix(
     p::Partition{Array{Int64, 2}}, r::Int64, s::Int64,
-    out_block_count_map, in_block_count_map, self_edge_weight::Int64
+    out_block_count_map, in_block_count_map, self_edge_weight::Int64,
+    count_log::CountLog
     )
+    #TODO: Figure out how to count edges traversed here.
     M_r_row = copy(p.M[r, :])
     M_r_col = copy(p.M[:, r])
     M_s_row = copy(p.M[s, :])
@@ -85,7 +95,7 @@ end
 """Computes the new rows and cols in `M`, when all nodes from `r` are shifted to
 block `s`."""
 function compute_new_matrix_agglomerative(
-    p::Partition{Array{Int64, 2}}, r::Int64, s::Int64
+    p::Partition{Array{Int64, 2}}, r::Int64, s::Int64, count_log::CountLog
     )
 
     M_r_row = zeros(Int64, p.B)
@@ -108,7 +118,7 @@ function compute_new_matrix_agglomerative(
 end
 
 function compute_multinomial_probs(
-    p::Partition{Array{Int64, 2}}, vertex::Int64
+    p::Partition{Array{Int64, 2}}, vertex::Int64, count_log::CountLog
     )
     return (p.M[:, vertex] .+ p.M[vertex, :]) ./ p.d[vertex]
 end
@@ -116,7 +126,8 @@ end
 function compute_delta_entropy(
     p::Partition{Array{Int64, 2}}, r::Int64, s::Int64,
     M_r_col::Vector{Int64}, M_s_col::Vector{Int64}, M_r_row::Vector{Int64},
-    M_s_row::Vector{Int64}, d_out_new::Vector{Int64}, d_in_new::Vector{Int64}
+    M_s_row::Vector{Int64}, d_out_new::Vector{Int64}, d_in_new::Vector{Int64},
+    count_log::CountLog
     )
     delta = 0.0
     # Sum over col of r in new M
@@ -163,7 +174,7 @@ end
 
 function compute_overall_entropy(
         M::Array{Int64, 2}, d_out::Vector{Int64}, d_in::Vector{Int64},
-        B::Int64, N::Int64, E::Int64
+        B::Int64, N::Int64, E::Int64, count_log::CountLog
     )
     rows, cols = findn(M)  # all non-zero entries
     summation_term = 0.0
@@ -180,7 +191,7 @@ end
 function compute_hastings_correction(
         s::Int64, p::Partition{Array{Int64, 2}}, M_r_row::Vector{Int64},
         M_r_col::Vector{Int64}, d_new::Vector{Int64},
-        blocks_out_count_map, blocks_in_count_map
+        blocks_out_count_map, blocks_in_count_map, count_log::CountLog
     )
     blocks = Set(keys(blocks_out_count_map)) ∪ Set(keys(blocks_in_count_map))
     p_forward = 0.0
@@ -196,7 +207,7 @@ end
 function update_partition(
     M::Array{Int64, 2}, r::Int64, s::Int64,
     M_r_col::Vector{Int64}, M_s_col::Vector{Int64},
-    M_r_row::Vector{Int64}, M_s_row::Vector{Int64}
+    M_r_row::Vector{Int64}, M_s_row::Vector{Int64}, count_log::CountLog
     )
     M[:, r] = M_r_col
     M[r, :] = M_r_row
