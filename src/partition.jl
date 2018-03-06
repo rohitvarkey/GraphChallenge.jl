6226a7fb7eaad4f669a3c6985d7938c7f1bd4935
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2018-03-06 17:28:07 -0500

    Turn knobs to 11

--- src/partition.jl
+++ src/partition.jl
@@ -268,17 +268,17 @@ function partition(T::Type, g::SimpleWeightedDiGraph, num_nodes::Int64)
     vertex_in_neighbors = inneighbors(g)
 
     β = 3
-    num_agg_proposals_per_block = 10 # number of proposals per block
+    num_agg_proposals_per_block = 100 # number of proposals per block
     num_block_reduction_rate = 0.5 # fraction of blocks to reduce until the golden ratio bracket is established
 
     # nodal partition updates parameters
     # maximum number of iterations
-    max_num_nodal_itr = 100
+    max_num_nodal_itr = 1000
     # stop iterating when the change in entropy falls below this fraction of the overall entropy
     # lowering this threshold results in more nodal update iterations and likely better performance, but longer runtime
-    delta_entropy_threshold1 = 5e-4
+    delta_entropy_threshold1 = 5e-8
     # threshold after the golden ratio bracket is established (typically lower to fine-tune to partition)
-    delta_entropy_threshold2 = 1e-7
+    delta_entropy_threshold2 = 1e-10
     # width of the moving average window for the delta entropy convergence criterion
     delta_entropy_moving_avg_window = 3
 
