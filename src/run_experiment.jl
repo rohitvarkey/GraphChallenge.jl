85af728212a90a3982b1be1ca7265524d0a110ce
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2018-03-15 01:04:53 -0400

    Rename timer

--- src/run_experiment.jl
+++ src/run_experiment.jl
@@ -4,7 +4,7 @@ using TimerOutputs
 function static_partition_experiment(T::Type, num_nodes::Int64; plot_file="blocks.svg")
     g = load_graph(num_nodes)
     timer = TimerOutput()
-    result = @timeit timer "T $(num_nodes)" partition(T, g, num_nodes, timer)
+    result = @timeit timer "timer" partition(T, g, num_nodes, timer)
     p, count_log = result
     truth = load_truth_partition(num_nodes)
     #draw(SVG(plot_file, 32cm, 32cm), plotbeliefs(g, p))
